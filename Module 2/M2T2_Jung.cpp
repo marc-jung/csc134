cc859ab7ea82ff6b1612f2ad65bb7bbdf97387ec
marc-jung <marcjung001@gmail.com>
2026-02-16 19:14:39 +0000

    rough outline

--- Module 2/M2T2_Jung.cpp
+++ Module 2/M2T2_Jung.cpp
@@ -14,17 +14,28 @@ int main() {
     //Making a receipt printer for a restaurant
     //declare all variables
     string meal_name = "Taco Plate";
-    double meal_price = 5.99;          
+    int num_meals;                      //how many they buy
+    double meal_price = 5.99; 
+    double sub_total;                   //price before tax/tips
     double tax_rate = 0.08;             //8% is 8/100 ("per cent")
     double tip_amt;                     //$ tipped
     double tax_amt;                     //$ of the actaul tax charges
-    double total_price;                 //meal + tip + tax
+    double total_price;                 //sub_total + tip + tax
 
     //get user input
+    cout << "Welcome to the CSC 134 Grill." << endl;
+    cout << "Today's Special: " << meal_name << endl;
+    cout << endl;
+    cout << "How many would you like? ";
+    cin >> num_meals;
 
     //do the calculation
+    sub_total = meal_price * num_meals;
 
     //present the output
-
+    cout << "YOUR ORDER" << endl << "----------------------" << endl;
+    cout << num_meals << " x " << meal_name << "\t$" << meal_price << endl;
+    cout << "Subtotal: \t$" << sub_total << endl;
+    
     return 0; // no errors
 }
