aa13d0f017b2bbaa748092e944307966cd186e23
marc-jung <marcjung001@gmail.com>
2026-02-16 18:57:23 +0000

    set variables

--- Module 2/M2T2_Jung.cpp
+++ Module 2/M2T2_Jung.cpp
@@ -11,6 +11,20 @@ Assumption: Sales tax is 8% (varies by county)
 using namespace std;
 
 int main() {
+    //Making a receipt printer for a restaurant
+    //declare all variables
+    string meal_name = "Taco Plate";
+    double meal_price = 5.99;          
+    double tax_rate = 0.08;             //8% is 8/100 ("per cent")
+    double tip_amt;                     //$ tipped
+    double tax_amt;                     //$ of the actaul tax charges
+    double total_price;                 //meal + tip + tax
+
+    //get user input
+
+    //do the calculation
+
+    //present the output
 
     return 0; // no errors
 }
