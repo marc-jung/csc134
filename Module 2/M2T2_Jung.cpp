5c985c18439e324f95d579cd8bcdf3de7af65268
marc-jung <marcjung001@gmail.com>
2026-02-16 18:52:31 +0000

    started m2t2

---
+++ Module 2/M2T2_Jung.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134
+M2T2 - Receipt Calculator
+Jung
+2026 FEB 16
+Goal: A correct looking receipt that handles sales tax.
+Assumption: Sales tax is 8% (varies by county)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0; // no errors
+}
