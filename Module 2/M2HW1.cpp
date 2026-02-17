fd7300f54eab22c5b21d694786c7d4c81f5b6865
marc-jung <marcjung001@gmail.com>
2026-02-17 13:48:46 +0000

    just started

--- Module 2/M2HW1.cpp
+++ Module 2/M2HW1.cpp
@@ -5,10 +5,13 @@ MARC JUNG
 2026 FEB 17
 */
 
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main() {
 
-    cout << "Question 1\n";
+    cout << "Question 1" << endl;
+    
     return 0;
 }
