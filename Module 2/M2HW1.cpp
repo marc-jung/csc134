427ba69808a76f8cace9a334c5e85156fc0e5997
marc-jung <marcjung001@gmail.com>
2026-02-17 13:57:07 +0000

    Asked for name

--- Module 2/M2HW1.cpp
+++ Module 2/M2HW1.cpp
@@ -10,8 +10,17 @@ MARC JUNG
 using namespace std;
 
 int main() {
+    //Write a program that simulates banking transactions using the following requirements.
+    cout << "Question 1 \n" << endl; 
+    //Declare variables needed
+    double start_act_bal, fin_act_bal;
+    double deposit, withdrawal; 
+    string act_name; 
+    string act_num; 
 
-    cout << "Question 1" << endl;
-    
+    //USER INPUT
+    cout << "Hello, please input your name below. \n" << act_name;
+    cin >> act_name; 
+
     return 0;
 }
