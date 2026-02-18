5eac7fc1fc7ae5a668624c3fc5c3b99837440f65
marc-jung <marcjung001@gmail.com>
2026-02-18 00:20:32 +0000

    deposit

--- Module 2/M2HW1.cpp
+++ Module 2/M2HW1.cpp
@@ -17,12 +17,13 @@ int main() {
     cout << "Question 1 \n" << endl; 
     //Declare variables needed
     cout << setprecision(2) << fixed;
-    long long start_act_bal, fin_act_bal;
+    double start_act_bal, fin_act_bal;
     double deposit, withdrawal; 
     string act_name; 
+    char yn, yn2;
 
     //USER INPUT
-    cout << "Hello, please input your first and last name below. \n" << act_name;
+    cout << "Hello, please input your first name below. \n" << act_name;
     cin >> act_name; 
 
     //generate a random account number
@@ -33,6 +34,41 @@ int main() {
     
     cout << "Bank Account Number: " << act_num << endl;
 
-    
+    //Ask the user what their account balance is
+    cout << "Please type in you account balance: $";
+    cin >> start_act_bal;
+
+    //Ask the user if they want to deposit or withdraw money from account
+    cout << "Hello " << act_name << ". Do you want to make a deposit? (y/n): ";
+    cin >> yn;
+    if (yn == 'n' || 'N')
+    {
+        cout << "Do you want to make a withdrawal? (y/n): "; 
+        cin >> yn2;
+        if (yn2 == 'y' || 'Y')
+        {
+            cout << "Please input how much you like to withdraw: $";
+            cin >> withdrawal;
+            fin_act_bal = start_act_bal - withdrawal;
+            cout << "Your account balance is $" << fin_act_bal;
+            cout << "\nHave a good day " << act_name;
+            return 0;
+        }
+        else 
+        {
+            fin_act_bal = start_act_bal;
+            cout << "Your account balance is $" << fin_act_bal;
+            cout << "\nHave a good day " << act_name;
+            return 0;
+        }
+    }
+    else if (yn == 'y' || 'Y')
+    {
+        cout << "Please input how much you like to deposit: ";
+        cin >> deposit;
+        fin_act_bal = start_act_bal + deposit;
+        cout << "Your account balance is: " << fin_act_bal;
+    }
+
     return 0;
 }
