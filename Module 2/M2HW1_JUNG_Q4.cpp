fa855597b4db7548478cbd9dfe4e9ec1e802f271
marc-jung <marcjung001@gmail.com>
2026-03-03 01:11:11 +0000

    final q4

--- Module 2/M2HW1_JUNG_Q4.cpp
+++ Module 2/M2HW1_JUNG_Q4.cpp
@@ -5,5 +5,26 @@ Question 4 School Chant
 */
 
 #include <iostream>
-#include <iomanip>
+#include <string>
+
 using namespace std;
+
+int main() {
+
+    string school, team, letsGo, cheerOne, cheerTwo;
+    letsGo = "Let's Go ";
+    school = " FTCC";
+    team = " Trojans";
+
+    cheerOne = letsGo + school;
+    cheerTwo = letsGo + team;
+
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+
+
+    return 0;
+}
